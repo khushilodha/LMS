15fc58de2511954a667a324bead980c3ebaf1e14
Khushi Lodha <95740285+khushilodha@users.noreply.github.com>
2023-12-13 09:50:48 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='LMS',
     version='1.0',
-    packages=[''],
+    packages=['']
     url='www.justdocodings.blogspot.com',
     license='Copyright (c), Manish Kumar',
     author='MANISH KUMAR',
